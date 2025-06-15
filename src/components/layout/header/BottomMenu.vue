2ec970bff8defa0f7aec19e13eb2ed875e4d52a9
Denis Inzuki <inzuki.nk@gmail.com>
2025-06-15 16:50:26 +0300

    feat: enhance BottomMenu component with solid and outline icon variants for improved visual feedback

--- src/components/layout/header/BottomMenu.vue
+++ src/components/layout/header/BottomMenu.vue
@@ -2,49 +2,75 @@
     <div class="btm-nav z-50 h-12 border-t">
         <router-link to="/">
             <button @click="scrollToTop">
-                <HomeIcon class="h-5 w-5" />
+                <HomeIconSolid v-if="isActive('/')" class="h-5 w-5" />
+                <HomeIconOutline v-else class="h-5 w-5" />
             </button>
         </router-link>
         <router-link to="/orders">
             <div class="indicator">
                 <span v-if="pendingOrdersCount > 0" class="indicator-item badge badge-primary badge-sm">{{ pendingOrdersCount }}</span>
                 <button @click="scrollToTop">
-                    <TruckIcon class="h-5 w-5" />
+                    <TruckIconSolid v-if="isActive('/orders')" class="h-5 w-5" />
+                    <TruckIconOutline v-else class="h-5 w-5" />
                 </button>
             </div>
         </router-link>
         <router-link to="/promotions">
             <button @click="scrollToTop">
-                <TagIcon class="h-5 w-5" />
+                <TagIconSolid v-if="isActive('/promotions')" class="h-5 w-5" />
+                <TagIconOutline v-else class="h-5 w-5" />
             </button>
         </router-link>
         <router-link to="/cart">
             <div class="indicator">
                 <span class="indicator-item badge badge-secondary badge-sm">{{ cartItemsCount }}</span>
                 <button @click="scrollToTop">
-                    <ShoppingCartIcon class="h-5 w-5" />
+                    <ShoppingCartIconSolid v-if="isActive('/cart')" class="h-5 w-5" />
+                    <ShoppingCartIconOutline v-else class="h-5 w-5" />
                 </button>
             </div>
         </router-link>
         <router-link to="/profile">
             <button @click="scrollToTop">
-                <UserIcon class="h-5 w-5" />
+                <UserIconSolid v-if="isActive('/profile')" class="h-5 w-5" />
+                <UserIconOutline v-else class="h-5 w-5" />
             </button>
         </router-link>
     </div>
 </template>
+
 <script>
-import { HomeIcon, TruckIcon, UserIcon, ShoppingCartIcon, TagIcon } from '@heroicons/vue/24/outline'
+import { 
+    HomeIcon as HomeIconOutline,
+    TruckIcon as TruckIconOutline,
+    UserIcon as UserIconOutline,
+    ShoppingCartIcon as ShoppingCartIconOutline,
+    TagIcon as TagIconOutline
+} from '@heroicons/vue/24/outline'
+
+import { 
+    HomeIcon as HomeIconSolid,
+    TruckIcon as TruckIconSolid,
+    UserIcon as UserIconSolid,
+    ShoppingCartIcon as ShoppingCartIconSolid,
+    TagIcon as TagIconSolid
+} from '@heroicons/vue/24/solid'
+
 import { useOrdersStore } from '@/stores/orders'
 
 export default {
     name: 'BottomMenu',
     components: {
-        HomeIcon,
-        TruckIcon,
-        UserIcon,
-        ShoppingCartIcon,
-        TagIcon
+        HomeIconOutline,
+        HomeIconSolid,
+        TruckIconOutline,
+        TruckIconSolid,
+        UserIconOutline,
+        UserIconSolid,
+        ShoppingCartIconOutline,
+        ShoppingCartIconSolid,
+        TagIconOutline,
+        TagIconSolid
     },
     props: {
         cartItemsCount: {
@@ -66,6 +92,9 @@ export default {
     methods: {
         scrollToTop() {
             window.scrollTo({ top: 0, behavior: 'smooth' })
+        },
+        isActive(route) {
+            return this.$route.path === route
         }
     }
 }
